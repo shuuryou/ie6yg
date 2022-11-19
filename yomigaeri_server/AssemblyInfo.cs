f86494f89ea842749fa6fbe684ec3a07d48380c7
shuuryou <36278767+shuuryou@users.noreply.github.com>
2022-11-19 23:27:30 +0100

    Backend: - New YGSERVER and YGSHARED projects (this commit only to prepare further repository setup)

---
+++ yomigaeri_server/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IE6 Yomigaeri Server")]
+[assembly: AssemblyDescription("IE6 Yomigaeri Server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/shuuryou")]
+[assembly: AssemblyProduct("IE6 Yomigaeri")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d474e0c5-ad74-416c-a8c1-52c6c4907c50")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
---
+++ yomigarei_shared/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IE6 Yomigaeri Backend Shared Library")]
+[assembly: AssemblyDescription("IE6 Yomigaeri Backend Shared Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/shuuryou")]
+[assembly: AssemblyProduct("IE6 Yomigaeri")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b9bceb27-082a-46c8-9015-25d6c2bdc4f2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
